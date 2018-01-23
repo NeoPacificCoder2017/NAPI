d3cb30f14f762783341e59f874b28b652f52e62b
Parea <taie.taataparea1@gmail.com>
2018-01-22 21:15:04 -1000

    migration créer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('id')->unique();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('pseudo');
+            $table->date('birthday');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->password('password');
             $table->timestamps();
         });
     }
