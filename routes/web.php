fa46983225d7f36cc3f47ca20942126ee0b8c502
Parea <taie.taataparea1@gmail.com>
2018-01-22 21:17:33 -1000

    routes créer

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', 'UserController@all');
+Route::get('user/new', 'UserController@new');
+Route::get('user/{userId}', 'UserController@show');
+Route::post('user', 'UserController@create');
+Route::get('user/{userId}/delete', 'UserController@destroy');
+//Route::get('users', 'UserController@all');
