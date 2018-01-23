089e74e5360265b68a1672832f733b4400326dae
Parea <taie.taataparea1@gmail.com>
2018-01-22 21:16:05 -1000

    Model créer

--- app/User.php
+++ app/User.php
@@ -14,16 +14,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['lastname', 'firstname', 'pseudo', 'birthday', 'email', 'password',];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password',];
 }
