57006ae4e2b5bf0a572c0cd412129b7a29f75ad3
gaby <chansaud.eremoana@gmail.com>
2018-01-24 10:56:03 -1000

    modification des fonctions du newCommentController

--- app/Http/Controllers/newCommentControler.php
+++ app/Http/Controllers/newCommentControler.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\newComment;
 
 class newCommentControler extends Controller
 {
     //
+    public function all(){
+
+        $comment = newComment::all();
+        
+        dump($comment);
+        return view('comments.comment');
+    }
+
+    public function show($commentId) {
+
+        $comment = newComment::find($commentId);
+        dump($comment);
+
+        return view('comments.comment');
+    }
+
+    public function new(){
+        return view('comments.comment-form');
+    }
+
+    public function create(Request $request){
+        $input = $request->all();
+        
+        $comment = new newComment();
+        $comment->user_id = $input['new_id'];
+        $comment->user_id = $input['user_id'];
+        $comment->comment = $input['comment'];
+        $comment->save();
+        
+
+    }
+
 }
