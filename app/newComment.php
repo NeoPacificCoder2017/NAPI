d1b0025f157ac5f7f358d06295a6877883f1d829
gaby <chansaud.eremoana@gmail.com>
2018-01-24 11:56:47 -1000

    ajout de la fonction getCommentsById dans le model newComment

--- app/newComment.php
+++ app/newComment.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class newComment extends Model
 {
     //
+    public function getCommentsByNewsId($newsId){
+        $newComment = newComment::find($newsId);
+    }
 }
