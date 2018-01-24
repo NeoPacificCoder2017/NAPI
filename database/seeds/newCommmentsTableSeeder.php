e2f5661c14754299744f3ace4a1d94fa18a4246c
gaby <chansaud.eremoana@gmail.com>
2018-01-23 22:56:07 -1000

    ajout des fonctions du seeds

--- database/seeds/newCommmentsTableSeeder.php
+++ database/seeds/newCommmentsTableSeeder.php
@@ -12,5 +12,28 @@ class newCommmentsTableSeeder extends Seeder
     public function run()
     {
         //
+        $newComments = [];
+
+        for($i = 0; $i < 10; $i++):
+            $newId = rand(1, 10);
+            $userId = rand(1, 10);
+            
+
+
+            $newComments[] = [
+                'new_id' => $newId, 
+                'user_id' => $userId,
+                'comment' => str_random(250),
+                
+            ];
+
+        endfor;
+
+        foreach ($newComments AS $newComment):
+
+            newComment::create($newComments);
+
+        endforeach;
+        
     }
 }
