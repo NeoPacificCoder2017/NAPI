98a6d3a799f2d0c5ac80ecf596258d727f1382d1
Parea <taie.taataparea1@gmail.com>
2018-01-24 16:44:09 -1000

    routes terminer

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('user/{userId}/delete', 'UserController@destroy');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
