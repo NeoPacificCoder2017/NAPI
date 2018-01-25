3686be98f751e4f4b1306ad6954fd454b7178bf9
gaby <chansaud.eremoana@gmail.com>
2018-01-24 16:29:52 -1000

    modification du Comments.bldae.php

--- resources/views/comments/Comments.blade.php
+++ resources/views/comments/Comments.blade.php
@@ -1,15 +1,16 @@
 @extends('template')
 
 @section('contenu')
-<h2>NAPI-Les commentaires</h2>
+<h2 class="text-center">NAPI-Les commentaires</h2>
   <div class="col-sm-offset-3 col-sm-6">
-	  <div class="panel panel-info">
+	  <div class="panel panel-body">
       <table class="table">
         <thead>
           <tr>
-            <th>#</th>
+            <th>check</th>
+            <th>#id</th>
             <th>new</th>
-            <th>pseudo</th>
+            <th>user</th>
             <th>comment</th>
             
           </tr>
@@ -20,13 +21,14 @@
               <td><input type="checkbox" name="checkbox"/></td>
               <td>#{{ $comment->id }}</td>
               <td>{{ $comment->new_id }}</td>
-              <td>{{ $comment->pseudo }}</td>
+              <td>{{ $comment->user_id }}</td>
               <td>{{ $comment->comment }}</td>
               
                 
             </tr>
           @endforeach
           <a href="comments/new" button class="btn btn-success pull-right btn-small"></a>
+          {{--  <button action="comments/new" type="button" class="btn btn-default navbar-btn">ajouter un commantaire</button>  --}}
         </tbody>
       </table>
     </div>
