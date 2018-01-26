7a97e8fd603e77fa9f2f279fa6e08c92b746b152
gaby <chansaud.eremoana@gmail.com>
2018-01-25 16:19:50 -1000

    ajout des fonctions du CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,4 +8,16 @@ use App\Http\Controllers\Controller;
 class CategoryController extends Controller
 {
     //
+    public function getAll(){
+        return view('categorys.list');
+    }
+
+    public function getAllActivated(){
+        return view('categorys.list');
+    }
+
+    public function editCateegory(){
+        return view('categorys.list');
+    }
+
 }
